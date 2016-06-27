ef6c3e910ede4a2fa6ba28f4e9cdefa177bd7bc4
John Shiver <johneshiver@gmail.com>
2016-06-26 21:33:04 -0400

    change static root

--- web/johnshiver_blog/settings.py
+++ web/johnshiver_blog/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
-STATIC_ROOT = '/var/www/johnshiver.org/static/'
+STATIC_ROOT = '/var/www/app/static/'
 STATIC_URL = '/static/'
 
 # Additional locations of static files
